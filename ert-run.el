7b64d49b55b98386c4295a7ed3583057332f93f3
Christian Ohler <ohler+emacs@fastmail.net>
2010-06-27 18:47:37 +1000

    Made number of empty lines before commentary section consistent across all files.

--- ert-run.el
+++ ert-run.el
@@ -19,7 +19,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see `http://www.gnu.org/licenses/'.
 
-
 ;;; Commentary:
 
 ;; This file is part of ERT, the Emacs Lisp Regression Testing tool.
