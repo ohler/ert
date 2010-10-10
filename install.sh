66485d4c89d92812647d054e16f8ab38f9e6cc58
Christian Ohler <ohler+emacs@fastmail.net>
2010-10-10 11:21:42 +0930

    New script install.sh that installs ERT into an Emacs bzr workspace.
    Also moved ert.texi from doc/lispref/ to doc/misc/ for now because
    proper integration with the Lisp manual probably requires some more
    work, at the very least replacing @chapter with @section, @section
    with @subsection etc., and we can do that work later.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+if [ $# != 1 ]; then
+   echo "usage: ./install.sh destdir"
+   echo
+   echo "Installs ERT into an Emacs bzr workspace.  The argument destdir must"
+   echo "be the root of that workspace."
+   exit 1
+fi
+destdir="$1"
+origdir="$(pwd)"
+
+lispfiles="$(find lisp -name \*.el)"
+testfiles="$(find test -name \*.el -o -name Makefile.in)"
+docfiles="$(find doc -name \*.texi)"
+
+cd -- "$destdir"
+
+if ! bzr diff -q > /dev/null; then
+    echo "There are pending changes in $(pwd).  Aborting."
+    exit 1
+fi
+
+mkdir -p test/automated
+
+for i in $lispfiles $testfiles $docfiles; do
+    cp -- "$origdir/$i" "$i"
+done
+bzr add $lispfiles $testfiles $docfiles
+
+patch -p0 < "$origdir/install.patch"
+
+autoreconf
